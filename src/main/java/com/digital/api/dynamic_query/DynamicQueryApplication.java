788a843e299cf295d7d705b71702522577ab5336
Ram Sadatkar <ramsadatkar@fynd.team>
2024-12-05 23:22:56 +0530

    updated h2 properties and pom with lambok dependency

--- src/main/java/com/digital/api/dynamic_query/DynamicQueryApplication.java
+++ src/main/java/com/digital/api/dynamic_query/DynamicQueryApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DynamicQueryApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DynamicQueryApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DynamicQueryApplication.class, args);
+    }
 
 }
