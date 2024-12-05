61148bca5a5ded9e28c54cb2c86961e42477b38a
Ram Sadatkar <ramsadatkar@fynd.team>
2024-12-05 14:43:52 +0530

    Springboot project

---
+++ src/main/java/com/digital/api/dynamic_query/DynamicQueryApplication.java
@@ -0,0 +1,13 @@
+package com.digital.api.dynamic_query;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicQueryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynamicQueryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/digital/api/dynamic_query/DynamicQueryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digital.api.dynamic_query;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamicQueryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
